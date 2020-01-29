a4c340165f0ccd1bab82935504992fff2c47d6c4
Gustavo Tsuji <gustavokt@gmail.com>
2020-01-29 10:59:39 -0300

    Update Forca.java

--- logicaDeProgramacao/2020/Forca.java
+++ logicaDeProgramacao/2020/Forca.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 import java.util.Random;
 
-class Forca {
+class Main {
   public static void main(String[] args) {
     String inputDoUsuario = "";
     Scanner leitor = new Scanner(System.in);
@@ -21,25 +21,35 @@ class Forca {
 
     int letrasAcertadas = 0;
     while(!inputDoUsuario.equals("sair")) {
+      
+      //Imprimindo o estado das letras que ja foram descobertas
       for(String b: palavraEscondida){
         System.out.print(b + " ");        
       }
       System.out.println("");
       
+      //Tentando descobrir uma letra da forca
       System.out.print("Digite: ");
       inputDoUsuario = leitor.next();
 
+      //Esta flag vai auxiliar na contagem de vidas do jogador
       boolean acertou = false;
       
+      //Varrer a palavra e verificar se a letra que o jogador passou esta
+      //na palavra que se deseja advinhar
       for(int i = 0; i < palavraParaAdvinhar.length(); i++){
-        //Fazerndo uma 
+        
+        //Fazendo uma conversão de char para String 
         String caracter = Character.toString(palavraParaAdvinhar.charAt(i));
         
         if(inputDoUsuario.equals(caracter)){
           acertou = true;
           letrasAcertadas++;
+          
+          //mostra a letra que foi advinhada
           palavraEscondida[i] = inputDoUsuario;
 
+          //verifica fim do jogo
           if(letrasAcertadas == palavraParaAdvinhar.length()){
             System.out.println("Você acertou a palavra!");
             return;
